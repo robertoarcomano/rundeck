b36ab5be94dca424597cb1de0493c9605df86807
Roberto Arcomano <bertolinux@gmail.com>
2020-08-27 22:54:04 +0100

    fixed wrong file name

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 docker run -d --name rundeck -p 4440:4440 rundeck/rundeck:3.3.2
 
 # 2. Customize rundeck container
-docker exec --user=root rundeck sed -ri "s/127.0.0.1:4440/rundeck.robertoarcomano.it/g" /etc/remco/templates/framework.properties /etc/remco/templates/rundeck-config.propertie
+docker exec --user=root rundeck sed -ri "s/127.0.0.1:4440/rundeck.robertoarcomano.it/g" /etc/remco/templates/framework.properties /etc/remco/templates/rundeck-config.properties
 
 # 3. Restart Container
 docker stop rundeck; docker start rundeck
