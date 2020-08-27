9828316d750b10d8934709fcd71ed7448c09ad50
Roberto Arcomano <bertolinux@gmail.com>
2020-08-27 22:52:29 +0100

    first version

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. Download and launch rundeck image
+docker run -d --name rundeck -p 4440:4440 rundeck/rundeck:3.3.2
+
+# 2. Customize rundeck container
+docker exec --user=root rundeck sed -ri "s/127.0.0.1:4440/rundeck.robertoarcomano.it/g" /etc/remco/templates/framework.properties /etc/remco/templates/rundeck-config.propertie
+
+# 3. Restart Container
+docker stop rundeck; docker start rundeck
+
